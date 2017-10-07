ae94c2b1133e1bd3ec470caadf8b6167432d856d
Shlomo Ariel <shlomoa@balink.net>
2017-10-07 11:47:04 +0300

    .

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ var mongoose = require('mongoose');
 var UserModel = require('./models/user');
 var RoleModel = require('./models/role');
 
+
+
+
 //------------------pasport
 var morgan = require('morgan');
 var passport = require('passport');
