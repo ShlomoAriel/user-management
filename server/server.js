7efa8013ac9e33c3050086320aa6eb0b88774528
chiptus <chiptus@gmail.com>
2017-10-11 11:36:59 -0500

    configure middleware

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 'use strict';
-var express = require('express');
-var app = express();
-// var jwt = require('express-jwt');
-var router = express.Router();
-var userManagement = require('../user-management-package');
-var mongoose = require('mongoose');
-
-//----------------------------------------------------------------------------------------------------------pasport
+const express = require('express');
+const app = express();
+const userManagement = require('../user-management-package');
+const mongoose = require('mongoose');
+const configMiddleware = require('./middleware');
 var config = require('./config/database'); // get db config file
-
+configMiddleware(app);
 mongoose.connect(config.database, { useMongoClient: true });
 
 var db = mongoose.connection;
@@ -17,9 +14,9 @@ userManagement.init(app, { db, secret: config.secret });
 
 app.listen(process.env.PORT || 3001, () => {
   //eslint-disable-next-line no-console
-  console.log('Listening on localhost 3001');
+  console.log('Listening on localhost', process.env.PORT || 3001);
 });
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
-app.use('/', router);
+// require('express-print-routes')(app, './routes.txt');
